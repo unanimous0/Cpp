12d90ee2f2a396e24f6e0039fe0c0c15cf79d800
unanimous0 <unanimous0@kaist.ac.kr>
2021-10-08 22:36:06 +0900

    Add files via upload

--- CppProgramming/Chapter1/Chapter1_1.cpp
+++ CppProgramming/Chapter1/Chapter1_1.cpp
@@ -1,9 +1,11 @@
-// Chapter 1   C++의 기초적인 사용법
-// Chapter 1.1 프로그램의 구조
+/*
+	Chapter 1   C++의 기초적인 사용법
+	Chapter 1.1 프로그램의 구조
+*/
 
 #include <iostream>		// Preprocessor Directive (전처리기)
 
-int main(void)
+int main1_1(void)
 {
 	int x = 2;
 	x = 5;
