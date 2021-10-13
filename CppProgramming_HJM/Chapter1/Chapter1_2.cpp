90b8f7c0f2a20ba5c96a1f7b257fe038192cb21e
unanimous0 <unanimous0@kaist.ac.kr>
2021-10-13 14:54:13 +0900

    Chapter 1-2 주석 comments

---
+++ CppProgramming_HJM/Chapter1/Chapter1_2.cpp
@@ -0,0 +1,22 @@
+/*
+	Chapter 1   C++의 기초적인 사용법
+	Chapter 1.2 주석 comments
+*/
+
+#include <iostream>		
+
+int main1_2(void)
+{
+	//주석 작성 커맨드: ctrl + k c
+	//주석 해제 커맨드: ctrl + k u
+
+	// int x = 2;
+	/* 
+	x = 5;
+	int y = x + 3; 
+	*/
+
+	/*std::cout << y << std::endl;*/
+
+	return 0;
+}
