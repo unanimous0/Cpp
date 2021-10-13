3a413f3f5940d7171dd3fd684fc310711b9e9001
unanimous0 <unanimous0@naver.com>
2021-10-13 15:15:50 +0900

    C++ 헤더파일 구조

--- CppEtc/HeaderProcess.cpp
+++ CppEtc/HeaderProcess.cpp
@@ -1,3 +1,40 @@
 /*
   C++ 헤더파일 만들기
 */
+
+# 1. 헤더파일 사용하지 않는 경우
+#include <iostream>
+
+using namespace std;
+
+class calculator
+{
+  public: void add()
+  {
+    int a, b;
+    cout << "a와 b를 차례대로 입력하세요." << endl;
+    cin >> a >> b;
+    cout << "결과: " << a + b << endl;
+  }
+  public: void subtract()
+  {
+    int a, b;
+    cout << "a와 b를 차례대로 입력하세요." << endl;
+    cin >> a >> b;
+    cout << "결과: " << a - b << endl;
+  }
+};
+
+int main()
+{
+ calculator c;
+  c.add();
+  c.subtract();
+  system("pause");
+  
+  return 0;
+}
+
+
+
+# 2. 헤더파일 사용하는 경우
