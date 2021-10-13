fc59034b6d2cd97f01c1861b3d94936d0c01bcc8
unanimous0 <unanimous0@naver.com>
2021-10-14 08:56:01 +0900

    c++ 헤더파일 구조

--- CppEtc/HeaderProcess.cpp
+++ CppEtc/HeaderProcess.cpp
@@ -2,39 +2,87 @@
   C++ 헤더파일 만들기
 */
 
-# 1. 헤더파일 사용하지 않는 경우
+// 1. 헤더파일 사용하지 않는 경우 - 클래스의 객체를 사용
+
+// main.cpp
+
 #include <iostream>
 
 using namespace std;
 
 class calculator
 {
-  public: void add()
-  {
-    int a, b;
-    cout << "a와 b를 차례대로 입력하세요." << endl;
-    cin >> a >> b;
-    cout << "결과: " << a + b << endl;
-  }
-  public: void subtract()
-  {
-    int a, b;
-    cout << "a와 b를 차례대로 입력하세요." << endl;
-    cin >> a >> b;
-    cout << "결과: " << a - b << endl;
-  }
+    public: void add()
+    {
+        int a, b;
+        cout << "Insert a and b" << endl;
+        cin >> a >> b;
+        cout << "Result: " << a + b << endl;
+    }
+    public: void subtract()
+    {
+        int a, b;
+        cout << "Insert a and b" << endl;
+        cin >> a >> b;
+        cout << "Result: " << a - b << endl;
+    }
 };
 
 int main()
 {
- calculator c;
-  c.add();
-  c.subtract();
-  system("pause");
-  
-  return 0;
+    calculator c;
+    c.add();
+    c.subtract();
+    system("pause");
+    
+    return 0;
 }
 
 
 
-# 2. 헤더파일 사용하는 경우
+// 2. 헤더파일 사용하는 경우 - 클래스를 헤더로 분리해서 확정성 있게 사용
+
+// calculator.h
+class calculator
+{
+    public: void add();
+    public: void subtract();
+};
+
+
+// calculator.cpp
+#include <iostream>
+#include "calculator.h"
+
+using namespace::std;
+
+void calculator::add()
+{
+    int a, b;
+    cout << "Insert a and b" << endl;
+    cin >> a >> b;
+    cout << "Result: " << a + b << endl;
+}
+
+void calculator::subtract()
+{
+    int a, b;
+    cout << "Insert a and b" << endl;
+    cin >> a >> b;
+    cout << "Result: " << a - b << endl;
+}
+
+
+// main.cpp
+#include "calculator.h"
+
+int main()
+{
+    calculator c;
+    c.add();
+    c.subtract();
+    
+    return 0;
+}
+
+
