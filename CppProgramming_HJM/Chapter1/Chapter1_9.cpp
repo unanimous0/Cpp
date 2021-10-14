439204c0cf1f2a490df7854d70fafe82db3ac326
unanimous0 <unanimous0@kaist.ac.kr>
2021-10-14 22:12:53 +0900

    Chapter 1-9 기본적인 서식 맞추기 (Formatting)

---
+++ CppProgramming_HJM/Chapter1/Chapter1_9.cpp
@@ -0,0 +1,20 @@
+/*
+	Chapter 1   C++의 기초적인 사용법
+	Chapter 1.9 기본적인 서식 맞추기 (Formatting)
+*/
+
+#include <iostream>
+
+int addd(int x, int y) { return x + y; }
+
+int main1_9()
+{
+	std::cout << "Hello, World " << "abcdef " <<
+		"Hello Home" << std::endl;
+
+	int my_y	   = 1;
+	int x		   = 4;
+	int num_apples = 123;
+
+	return 0;
+}
