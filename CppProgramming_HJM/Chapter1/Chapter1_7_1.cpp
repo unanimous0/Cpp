28f54ae0f3fef15099fe118efc47846396af68e7
unanimous0 <unanimous0@kaist.ac.kr>
2021-10-14 22:11:48 +0900

    Chapter 1-7-1 지역 범위 예제

---
+++ CppProgramming_HJM/Chapter1/Chapter1_7_1.cpp
@@ -0,0 +1,28 @@
+/*
+	Chapter 1	  C++의 기초적인 사용법
+	Chapter 1.7.1 지역 범위 예제 
+*/
+
+#include <iostream>
+
+void doSomething(int x)
+{
+	x = 123;
+	std::cout << "No 2 Value: " << x << std::endl;
+	std::cout << "No 2 Address: " << &x << std::endl << std::endl;;
+}
+
+int main1_7_1()
+{
+	int x = 0;
+
+	std::cout << "No 1 Value: " << x << std::endl;
+	std::cout << "No 1 Address: " << &x << std::endl << std::endl;
+
+	doSomething(x);
+
+	std::cout << "No 3 Value: " << x << std::endl;
+	std::cout << "No 3 Address: " << &x << std::endl << std::endl;
+
+	return 0;
+}
