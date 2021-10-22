0dd633265dab878aa2cf689bbaaaac55ce50a23f
unanimous0 <unanimous0@naver.com>
2021-10-22 21:38:55 +0900

    Chapter 2-3 고정 너비 정수 (Fixed-width Integers, C++11)

---
+++ CppProgramming_HJM/Chapter2/Chapter2_3.cpp
@@ -0,0 +1,24 @@
+/*
+	Chapter 2   변수의 기본 자료형
+	Chapter 2.3 고정 너비 정수 (Fixed-width Integers, C++ 11)
+*/
+
+#include <iostream>	
+//#include <cstdint>		            // 사실 iostream에서 cstdint를 include 하고 있기 때문에 iostream을 불렀다면 안써도 됨
+
+int main2_3()
+{
+	std::int16_t i(5);			          // 16bits = 2Bytes -> 마우스 갖다 대면 typedef short 라고 뜸 (int 아님)
+	std::int8_t myint = 65;		        //  8bits = 1Bytes -> 마우스 갖다 대면 typedef signed char 라고 뜸 (int 아님)
+
+	std::cout << i << std::endl;
+	std::cout << myint << std::endl;
+
+	std::int_fast8_t fi(5);		        // 8Bits 사이즈 중에 가장 빠른 타입 -> typedef signed char
+	std::int_least64_t fl(5);	        // 적어도 8Bytes를 갖는 타입 -> typedef long long
+
+	std::cout << fi << std::endl;
+	std::cout << fl << std::endl;
+
+	return 0;
+}
